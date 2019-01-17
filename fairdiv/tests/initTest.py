e2582349b0a31b473668d43a999e104fc8517662
tkchouaki <tkchouaki@icloud.com>
2019-01-17 11:13:14 +0100

    testing is_ordinally_less

--- fairdiv/tests/initTest.py
+++ fairdiv/tests/initTest.py
@@ -2,39 +2,42 @@ from fairdiv import *
 
 
 if __name__ == "__main__":
-    # Should be an even number at least equal to 10
-    number_of_goods = 20
+    for j in range(9):
+        number_of_goods = (j+1) * 2
 
-    goods = [Good(str(i)) for i in range(number_of_goods)]
+        print("testing with " + str(number_of_goods))
+        goods = [Good(str(i)) for i in range(number_of_goods)]
 
-    even_goods = [goods[i] for i in range(number_of_goods) if i % 2 == 0]
-    odd_goods = [goods[i] for i in range(number_of_goods) if i % 2 == 1]
+        even_goods = [goods[i] for i in range(number_of_goods) if i % 2 == 0]
+        odd_goods = [goods[i] for i in range(number_of_goods) if i % 2 == 1]
 
-    a1 = Agent("agent1", goods[:])
-    a2 = Agent("agent2", goods[::-1])
-    assert a1.top() == goods[0]
-    assert a1.sb() == goods[1]
-    assert a1.last() == goods[number_of_goods-1]
-    assert a2.top() == goods[number_of_goods-1]
-    assert a2.sb() == goods[number_of_goods-2]
-    assert a2.last() == goods[0]
+        a1 = Agent("agent1", goods[:])
+        a2 = Agent("agent2", goods[::-1])
+        assert a1.top() == goods[0]
+        assert a1.sb() == goods[1]
+        assert a1.last() == goods[number_of_goods-1]
+        assert a2.top() == goods[number_of_goods-1]
+        assert a2.sb() == goods[number_of_goods-2]
+        assert a2.last() == goods[0]
 
-    assert a1.top(even_goods) == goods[0]
-    assert a1.sb(even_goods) == goods[2]
-    assert a1.last(even_goods) == goods[number_of_goods-2]
-    assert a2.top(even_goods) == goods[number_of_goods-2]
-    assert a2.sb(even_goods) == goods[number_of_goods-4]
-    assert a2.last(even_goods) == goods[0]
+        assert a1.top(even_goods) == goods[0]
+        if number_of_goods >= 4:
+            assert a1.sb(even_goods) == goods[2]
+        assert a1.last(even_goods) == goods[number_of_goods-2]
+        assert a2.top(even_goods) == goods[number_of_goods-2]
+        if number_of_goods >= 4:
+            assert a2.sb(even_goods) == goods[number_of_goods-4]
+        assert a2.last(even_goods) == goods[0]
 
-    assert a1.rank(a1.top()) == 1
-    assert a2.rank(a1.top()) == number_of_goods
+        assert a1.rank(a1.top()) == 1
+        assert a2.rank(a1.top()) == number_of_goods
 
-    assert a1.h(even_goods, number_of_goods//2) == [goods[i] for i in range(number_of_goods) if i % 2 == 0 and i < number_of_goods//2]
-    assert a2.h(even_goods, number_of_goods//2) == [goods[i] for i in reversed(range(number_of_goods)) if i % 2 == 0 and i >= number_of_goods//2]
+        assert a1.h(even_goods, number_of_goods//2) == [goods[i] for i in range(number_of_goods) if i % 2 == 0 and i < number_of_goods//2]
+        assert a2.h(even_goods, number_of_goods//2) == [goods[i] for i in reversed(range(number_of_goods)) if i % 2 == 0 and i >= number_of_goods//2]
 
-    assert max_min_rank([a1, a2], goods) == number_of_goods//2
+        assert max_min_rank([a1, a2], goods) == number_of_goods//2
 
-    assert a1.is_ordinally_less(odd_goods)
-    assert not a1.is_ordinally_less(even_goods)
-    assert a2.is_ordinally_less(even_goods)
-    assert not a2.is_ordinally_less(odd_goods)
+        assert a1.is_ordinally_less(odd_goods)
+        assert not a1.is_ordinally_less(even_goods)
+        assert a2.is_ordinally_less(even_goods)
+        assert not a2.is_ordinally_less(odd_goods)

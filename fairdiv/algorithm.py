960b54813d187e14d9dc4e8dc267f85698149907
tkchouaki <tkchouaki@icloud.com>
2019-01-22 11:44:42 +0100

    Bug fix restricted sequential

--- fairdiv/algorithm.py
+++ fairdiv/algorithm.py
@@ -76,30 +76,35 @@ def restricted_sequential(agents, goods):
         if len(u) == 0:
             allocations.add((tuple(z[0]), tuple(z[1])))
             return
-
-        ha_l = [p for p in agents[0].preferences[:l] if p in u]
-        hb_l = [p for p in agents[1].preferences[:l] if p in u]
-
-        if ha_l[0] != hb_l[0]:
+        top_a = agents[0].top(u)
+        top_b = agents[1].top(u)
+        branched = False
+        if top_a != top_b:
             za, zb = z[0][:], z[1][:]
-            v = [good for good in u if good != ha_l[0] and good != hb_l[0]]
-            za.append(ha_l[0])
-            zb.append(hb_l[0])
+            v = [good for good in u if good != top_a and good != top_b]
+            za.append(top_a)
+            zb.append(top_b)
             inner((za, zb), v, l+1)
+            branched = True
         else:
-            if len(ha_l) > 1:
+            ha_l = agents[0].h(u, l)
+            hb_l = agents[1].h(u, l)
+            if len(ha_l) > 1 and len(hb_l) > 0:
                 za, zb = z[0][:], z[1][:]
                 v = [good for good in u if good != ha_l[1] and good != hb_l[0]]
                 za.append(ha_l[1])
                 zb.append(hb_l[0])
                 inner((za, zb), v, l + 1)
-            if len(hb_l) > 1:
+                branched = True
+            if len(hb_l) > 1 and len(ha_l) > 0:
                 za, zb = z[0][:], z[1][:]
                 v = [good for good in u if good != ha_l[0] and good != hb_l[1]]
                 za.append(ha_l[0])
                 zb.append(hb_l[1])
                 inner((za, zb), v, l + 1)
-
+                branched = True
+        if not branched:
+            inner(z, u, l+1)
     inner(([], []), goods, 1)
     return Allocation.get_allocations(agents, allocations)
 
@@ -220,22 +225,22 @@ def trump_algorithm(agents, goods):
 if __name__ == '__main__':
     import statistics
 
-    goods = [Good(str(i)) for i in range(4)]
+    goods = [Good(str(i)) for i in range(6)]
 
     a1 = Agent("A")
-    a1.preferences = [
-        goods[0], goods[2], goods[1], goods[3]
-    ]
+    a1.preferences = goods[:]
 
     a2 = Agent("B")
     a2.preferences = [
-        goods[1], goods[3], goods[0], goods[2]
+        goods[2], goods[3], goods[4], goods[5], goods[0], goods[1]
     ]
+
 
     # allocs = singles_doubles((a1, a2), goods)
     # allocs = original_sequential((a1, a2), goods)
-    allocs = trump_algorithm((a1, a2), goods)
+    # allocs = trump_algorithm((a1, a2), goods)
     # allocs = bottom_up((a1, a2), goods)
+    allocs = restricted_sequential((a1, a2), goods)
     all_allocs = Allocation.generate_all_allocations((a1, a2), goods)
 
     statistics.print_statistics(statistics.gather_data((a1, a2), allocs, all_allocs))

e4a0fd1fc54bac903da4019b681ddc6da7fda932
tkchouaki <tkchouaki@icloud.com>
2019-01-22 16:55:13 +0100

    generate_possible_problems

--- fairdiv/__init__.py
+++ fairdiv/__init__.py
@@ -176,17 +176,22 @@ class Utils(object):
     @staticmethod
     @cache
     def generate_possible_problems(problems_size=2, envy_free_only=False):
-        problems = []
+        problems = list()
         goods = [Good(str(i)) for i in range(problems_size)]
-        for preferences_b in itertools.permutations(goods):
-            a = Agent("A", goods[:])
-            b = Agent("B", list(preferences_b))
-            problem = ((a, b), goods)
-            if envy_free_only:
+        if not envy_free_only:
+            for preferences_b in itertools.permutations(goods):
+                a = Agent("A", goods[:])
+                b = Agent("B", list(preferences_b))
+                problem = ((a, b), goods)
+                problems.append(problem)
+        else:
+            all_problems = Utils.generate_possible_problems(problems_size)
+            for problem in all_problems:
                 if len(algorithm.trump_algorithm(*problem)) == 0:
                     continue
-            problems.append(problem)
+                problems.append(problem)
         return problems
+
 
 @total_ordering
 class Good(object):

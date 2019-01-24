6727bf758e06a15672ce285128f3951c8789e6da
tkchouaki <tkchouaki@icloud.com>
2019-01-24 09:57:44 +0100

    bug fix generate_possible_problems

--- fairdiv/problemGenerators.py
+++ fairdiv/problemGenerators.py
@@ -5,19 +5,16 @@ import itertools
 
 
 @cache
-def generate_possible_problems(problems_size=2, envy_free_only=False):
+def generate_possible_problems(problems_size=2):
+    """
+    :param problems_size: The size of the desired problems (should an even number)
+    :return: All the possible problems of the given size
+    """
     problems = list()
     goods = [Good(str(i)) for i in range(problems_size)]
-    if not envy_free_only:
-        for preferences_b in itertools.permutations(goods):
-            a = Agent("A", goods[:])
-            b = Agent("B", list(preferences_b))
-            problem = ((a, b), goods)
-            problems.append(problem)
-    else:
-        all_problems = generate_possible_problems(problems_size)
-        for problem in all_problems:
-            if len(trump_algorithm(*problem)) == 0:
-                continue
-            problems.append(problem)
+    for preferences_b in itertools.permutations(goods):
+        a = Agent("A", goods[:])
+        b = Agent("B", list(preferences_b))
+        problem = ((a, b), goods)
+        problems.append(problem)
     return problems
